01420a96b2e74a6730191c18e10b729d55537f08
ohsnaplol <emyb@bellsouth.net>
2017-11-30 16:39:57 -0500

    ability to add tags

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 var tagArray = ["dog", "cat"]
 
 $(document).ready(function() {
+  for (var i = 0; i < tagArray.length; i++) {
+    addTagToPage(tagArray[i]);
+  }
 
+  $("#addBtn").on("click", function () {
+    inputValue = $("input").val()
+    if (!tagArray.includes(inputValue) && inputValue !== "") {
+      tagArray.push(inputValue)
+      addTagToPage(inputValue);
+    }
+  })
+})
+
+function addTagToPage(title) {
+  $("#tagList").append('<button class="btn" id="' + title + '">' + title + '</button>');
 }

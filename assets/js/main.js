547fba07f2a6ae0a87b473b41538c657dd9c15f4
ohsnaplol <emyb@bellsouth.net>
2017-12-29 12:41:52 -0500

    Added form id and prevented form default behavior

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,10 @@ $(document).ready(function() {
       this.play();
     }
   });
+
+  $("#tagAdder").on('submit', function(event) {
+    event.preventDefault()
+  })
 })
 
 function addGifs(title) {
